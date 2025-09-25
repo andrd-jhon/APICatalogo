512b02deb0f3c4bd76cc5d594ce31342a58978ed
andrd-jhon <jv-andrade02@outlook.com>
2025-09-25 13:37:51 -0300

    Criação de models; Configuração EF Core; Criação de classe de contexto do banco de dados.

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+        }
+
+        public DbSet <Categoria> categorias { get; set; }
+        public DbSet <Produto> produtos { get; set; }
+    }
+}
