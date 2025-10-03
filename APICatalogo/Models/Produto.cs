3f57f047672935c5e6074651a116074bd981b0ce
andrd-jhon <jv-andrade02@outlook.com>
2025-10-03 12:30:34 -0300

    Testes de atributos dos Data Annotations.

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -9,17 +9,18 @@ namespace APICatalogo.Models
     {
         [Key]
         public int ProdutoId { get; set; }
-        [Required]
-        [StringLength(80)]
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 carácteres.", MinimumLength = 5)]
         public string? Nome { get; set; }
         [Required]
-        [StringLength(300)]
+        [StringLength(10, ErrorMessage = "A descrição deve conter no mínimo {1} caracteres.")]
         public string? Descricao { get; set; }
         [Required]
         [Column(TypeName = "decimal(10,2)")]
+        [Range(1, 1000, ErrorMessage = "O Preço deve estar entre o valor {1} e {2}.")]
         public decimal Preco { get; set; }
         [Required]
-        [StringLength(300)]
+        [StringLength(300, MinimumLength = 10)]
         public string? ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
