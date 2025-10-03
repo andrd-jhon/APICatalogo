3c37f26c95b691b9e029d5517af4eb8ee4c2f894
andrd-jhon <jv-andrade02@outlook.com>
2025-10-03 19:04:49 -0300

    Validação personalizada com método Validate() na própria model.

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -6,13 +6,13 @@ using System.Text.Json.Serialization;
 namespace APICatalogo.Models
 {
     [Table("Produtos")]
-    public class Produto
+    public class Produto : IValidatableObject
     {
         [Key]
         public int ProdutoId { get; set; }
         [Required(ErrorMessage = "O nome é obrigatório.")]
         [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 carácteres.", MinimumLength = 5)]
-        [PrimeiraLetraMaiuscula]
+        //[PrimeiraLetraMaiuscula]
         public string? Nome { get; set; }
         [Required]
         [StringLength(10, ErrorMessage = "A descrição deve conter no mínimo {1} caracteres.")]
@@ -30,5 +30,22 @@ namespace APICatalogo.Models
 
         [JsonIgnore]
         public Categoria? Categoria { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationcontext)
+        {
+            if (!string.IsNullOrEmpty(this.Nome))
+            {
+                var primeiraLetra = this.Nome[0].ToString();
+                if (primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("A primeira letra deve ser maiúscula.", new[] {nameof(this.Nome)});
+                }
+
+            }
+
+            if (this.Estoque <= 0)
+            {
+                yield return new ValidationResult("O estoque deve ser maior que zero.", new[] { nameof(this.Nome) });
+            }
+        }
     }
 }
