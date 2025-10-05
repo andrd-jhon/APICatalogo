06628561637f86c5f1c5c6c21cf858f987cca13e
andrd-jhon <jv-andrade02@outlook.com>
2025-10-04 22:52:57 -0300

    Criação de um log personalizado e registrando em arquivo local.

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,6 +1,7 @@
 using APICatalogo.Context;
 using APICatalogo.Extensions;
 using APICatalogo.Filters;
+using APICatalogo.Logging;
 using APICatalogo.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -34,6 +35,11 @@ builder.Services.Configure<ApiBehaviorOptions>(options => /*Esse trecho de codig
 {
     options.DisableImplicitFromServicesParameters = true;
 });
+
+builder.Logging.AddProvider(new CustomLoggerProvider(new CustomLoggerProviderConfiguration
+{
+    LogLevel = LogLevel.Information,
+}));
 
 var app = builder.Build();
 
