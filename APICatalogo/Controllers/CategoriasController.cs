c0c1af3193a8b09ca0c09c2913c341d0c5cd626c
andrd-jhon <jv-andrade02@outlook.com>
2025-10-08 11:32:11 -0300

    Mapeando DTOs manualmente em CategoriasController.

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Context;
+using APICatalogo.DTOs;
 using APICatalogo.Filters;
 using APICatalogo.Interfaces;
 using APICatalogo.Models;
@@ -22,29 +23,79 @@ namespace APICatalogo.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Categoria>> Get()
+        public ActionResult<IEnumerable<CategoriaDTO>> Get()
         {
-            return Ok(_unityOfWork.CategoriaRepository.GetAll());
+            var categorias = _unityOfWork.CategoriaRepository.GetAll();
+
+            if (categorias is null)
+                return NotFound();
+
+            var categoriasDTO = new List<CategoriaDTO>();
+
+            foreach (var categoria in categorias)
+            {
+                var categoriaDTO = new CategoriaDTO()
+                {
+                    CategoriaId = categoria.CategoriaId,
+                    Nome = categoria.Nome,
+                    ImagemUrl = categoria.ImagemUrl
+                };
+
+                categoriasDTO.Add(categoriaDTO);
+            }
+
+            return Ok();
         }
 
         [HttpGet("{id:int}", Name = "ObterCategoria")]
-        public ActionResult<Categoria> Get(int id)
+        public ActionResult<CategoriaDTO> Get(int id)
         {
-            return Ok(_unityOfWork.CategoriaRepository.Get(p => p.CategoriaId == id));
+            var categoria = _unityOfWork.CategoriaRepository.Get(p => p.CategoriaId == id);
+
+            //MAPEAMENTO MANUAL DE DTO
+            var categoriaDTO = new CategoriaDTO()
+            {
+                CategoriaId = categoria.CategoriaId,
+                Nome = categoria.Nome,
+                ImagemUrl = categoria.ImagemUrl
+            };
+
+            return Ok(categoriaDTO);
         }
 
         [HttpPost]
-        public ActionResult Post(Categoria categoria)
+        public ActionResult<CategoriaDTO> Post(CategoriaDTO categoriaDTO)
         {
-            _unityOfWork.CategoriaRepository.Create(categoria);
+            var categoria = new Categoria()
+            {
+                CategoriaId = categoriaDTO.CategoriaId,
+                Nome = categoriaDTO.Nome,
+                ImagemUrl = categoriaDTO.ImagemUrl
+            };
+
+            var categoriaCriada = _unityOfWork.CategoriaRepository.Create(categoria);
             _unityOfWork.Commit();
 
-            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+            var retornoCategoriaDTO = new CategoriaDTO()
+            {
+                CategoriaId = categoriaCriada.CategoriaId,
+                Nome = categoriaCriada.Nome,
+                ImagemUrl = categoriaCriada.ImagemUrl
+            };
+
+            return new CreatedAtRouteResult("ObterCategoria", new { id = retornoCategoriaDTO.CategoriaId }, retornoCategoriaDTO);
         }
 
         [HttpPut]
-        public ActionResult Put(Categoria categoria)
+        public ActionResult<CategoriaDTO> Put(CategoriaDTO categoriaDTO)
         {
+            var categoria = new Categoria()
+            {
+                CategoriaId = categoriaDTO.CategoriaId,
+                Nome = categoriaDTO.Nome,
+                ImagemUrl = categoriaDTO.ImagemUrl
+            };
+
             _unityOfWork.CategoriaRepository.Update(categoria);
             _unityOfWork.Commit();
 
@@ -52,12 +103,19 @@ namespace APICatalogo.Controllers
         }
 
         [HttpDelete("{id:int}")]
-        public ActionResult Delete(int id)
+        public ActionResult<CategoriaDTO> Delete(int id)
         {
             var categoria = _unityOfWork.CategoriaRepository.Delete(id);
             _unityOfWork.Commit();
 
-            return Ok(categoria);
+            var categoriaDTO = new CategoriaDTO()
+            {
+                CategoriaId = categoria.CategoriaId,
+                Nome = categoria.Nome,
+                ImagemUrl = categoria.ImagemUrl
+            };
+
+            return Ok(categoriaDTO);
         }
     }
 }
