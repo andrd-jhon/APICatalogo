b378a5e018b074cd833a6c7411ae48c4ac1238d8
andrd-jhon <jv-andrade02@outlook.com>
2025-10-18 16:31:21 -0300

    Utilizando métodos de extensão para mapear DTO's e Models.

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -1,7 +1,9 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.DTOs;
+using APICatalogo.DTOs.Mappings;
 using APICatalogo.Filters;
 using APICatalogo.Interfaces;
+using APICatalogo.Logging;
 using APICatalogo.Models;
 using APICatalogo.Services;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +18,7 @@ namespace APICatalogo.Controllers
     public class CategoriasController : ControllerBase
     {
         private readonly IUnitOfWork _unityOfWork;
+        private readonly CustomerLogger _logger;
 
         public CategoriasController(IUnitOfWork unityOfWork)
         {
@@ -30,21 +33,9 @@ namespace APICatalogo.Controllers
             if (categorias is null)
                 return NotFound();
 
-            var categoriasDTO = new List<CategoriaDTO>();
+            var categoriasDTO = categorias.ToCategoriaDTOList();
 
-            foreach (var categoria in categorias)
-            {
-                var categoriaDTO = new CategoriaDTO()
-                {
-                    CategoriaId = categoria.CategoriaId,
-                    Nome = categoria.Nome,
-                    ImagemUrl = categoria.ImagemUrl
-                };
-
-                categoriasDTO.Add(categoriaDTO);
-            }
-
-            return Ok();
+            return Ok(categoriasDTO);
         }
 
         [HttpGet("{id:int}", Name = "ObterCategoria")]
@@ -55,13 +46,7 @@ namespace APICatalogo.Controllers
             if (categoria is null)
                 return StatusCode(404);
 
-            //MAPEAMENTO MANUAL DE DTO
-            var categoriaDTO = new CategoriaDTO()
-            {
-                CategoriaId = categoria.CategoriaId,
-                Nome = categoria.Nome,
-                ImagemUrl = categoria.ImagemUrl
-            };
+            var categoriaDTO = categoria.ToCategoriaDTO();
 
             return Ok(categoriaDTO);
         }
@@ -69,22 +54,19 @@ namespace APICatalogo.Controllers
         [HttpPost]
         public ActionResult<CategoriaDTO> Post(CategoriaDTO categoriaDTO)
         {
-            var categoria = new Categoria()
+            if (categoriaDTO is null)
             {
-                CategoriaId = categoriaDTO.CategoriaId,
-                Nome = categoriaDTO.Nome,
-                ImagemUrl = categoriaDTO.ImagemUrl
-            };
+                _logger.LogWarning($"Dados inválidos.");
+                return BadRequest();
+            }
+
+            var categoria = categoriaDTO.ToCategoria();
 
             var categoriaCriada = _unityOfWork.CategoriaRepository.Create(categoria);
+
             _unityOfWork.Commit();
 
-            var retornoCategoriaDTO = new CategoriaDTO()
-            {
-                CategoriaId = categoriaCriada.CategoriaId,
-                Nome = categoriaCriada.Nome,
-                ImagemUrl = categoriaCriada.ImagemUrl
-            };
+            var retornoCategoriaDTO = categoriaCriada.ToCategoriaDTO();
 
             return new CreatedAtRouteResult("ObterCategoria", new { id = retornoCategoriaDTO.CategoriaId }, retornoCategoriaDTO);
         }
@@ -92,47 +74,37 @@ namespace APICatalogo.Controllers
         [HttpPut]
         public ActionResult<CategoriaDTO> Put(CategoriaDTO categoriaDTO)
         {
-            var categoria = new Categoria()
+            if (categoriaDTO is null)
             {
-                CategoriaId = categoriaDTO.CategoriaId,
-                Nome = categoriaDTO.Nome,
-                ImagemUrl = categoriaDTO.ImagemUrl
-            };
+                _logger.LogWarning($"Dados inválidos.");
+                return BadRequest();
+            }
+
+            var categoria = categoriaDTO.ToCategoria();
 
             _unityOfWork.CategoriaRepository.Update(categoria);
             _unityOfWork.Commit();
 
-            return Ok(categoria);
+            var categoriaAtualizadaDTO = categoria.ToCategoriaDTO();
+
+            return Ok(categoriaAtualizadaDTO);
         }
 
         [HttpDelete("{id:int}")]
         public ActionResult<CategoriaDTO> Delete(int id)
         {
-            var categoria = _unityOfWork.CategoriaRepository.Delete(id);
+            var categoria = _unityOfWork.CategoriaRepository.Get(p => p.CategoriaId == id);
+
+            if (categoria is null)
+            {
+                _logger.LogWarning($"Dados inválidos.");
+                return BadRequest();
+            }
             _unityOfWork.Commit();
 
-            var categoriaDTO = new CategoriaDTO()
-            {
-                CategoriaId = categoria.CategoriaId,
-                Nome = categoria.Nome,
-                ImagemUrl = categoria.ImagemUrl
-            };
+            var categoriaDTO = categoria.ToCategoriaDTO();
 
             return Ok(categoriaDTO);
         }
-
-        [HttpGet("Teste")]
-        public ActionResult TesteFront()
-        {
-            return Ok(new { mensagem = "teste" });
-        }
-
-        [HttpPost("Teste")]
-        public ActionResult TesteFront(string nome)
-        {
-            return Ok(new { mensagem = "teste"});
-        }
-
     }
-
 }
