4c9e1f2c36b9a7c5874b4ad42d0bb6ae2be8edd6
andrd-jhon <jv-andrade02@outlook.com>
2026-01-06 12:23:03 -0300

    Método GenerateRefreshToken.

--- APICatalogo/Services/TokenService.cs
+++ APICatalogo/Services/TokenService.cs
@@ -34,7 +34,15 @@ namespace APICatalogo.Services
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var secureRandomBytes = new byte[128];
+
+            using var randomNumberGenerator = RandomNumberGenerator.Create();
+
+            randomNumberGenerator.GetBytes(secureRandomBytes);
+
+            var refreshToken = Convert.ToBase64String(secureRandomBytes);
+
+            return refreshToken;
         }
 
         public ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config)
