8d290a8037f3c3bb2f09187916e78e9063b477af
andrd-jhon <jv-andrade02@outlook.com>
2026-01-28 21:46:03 -0300

    Utilizando aatributo MapToApiVersion para acessar diferentes endpoints em uma controller.

---
+++ APICatalogo/Controllers/TesteV3Controller.cs
@@ -0,0 +1,27 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/teste")]
+    [ApiController]
+    [ApiVersion(3)]
+    [ApiVersion(4)]
+    public class TesteV3Controller : ControllerBase
+    {
+        [MapToApiVersion(3)]
+        [HttpGet]
+        public string GetVersionV3()
+        {
+            return "versão 3";
+        }
+
+        [MapToApiVersion(4)]
+        [HttpGet]
+        public string GetVersionV4()
+        {
+            return "versão 4";
+        }
+    }
+}
