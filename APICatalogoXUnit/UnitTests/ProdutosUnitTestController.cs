625b1dab2f842c7316f1ae0195514857818db6ac
andrd-jhon <jv-andrade02@outlook.com>
2026-02-02 18:48:45 -0300

    Criação de projeto XUnit para testes unitários; Criação de classe de configuração das opções de banco e repositório para testes em ProdutosController.

---
+++ APICatalogoXUnit/UnitTests/ProdutosUnitTestController.cs
@@ -0,0 +1,43 @@
+﻿using APICatalogo.Context;
+using APICatalogo.DTOs.Mappings;
+using APICatalogo.Interfaces;
+using APICatalogo.Repositories;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICatalogoXUnit.UnitTests
+{
+    public class ProdutosUnitTestController
+    {
+        private IUnitOfWork repository;
+        private IMapper mapper;
+        public static DbContextOptions<AppDbContext> dbContextOptions {  get; }
+        public static string connectionString = "Server=localhost;Database=APICatalogoDB;User Id=root;Password=103005@Mysql;";
+
+        static ProdutosUnitTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                .Options;
+        }
+
+        public ProdutosUnitTestController()
+        {
+            var config = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new ProdutoDTOMappingProfile());
+            });
+
+            mapper = config.CreateMapper();
+
+            var context = new AppDbContext(dbContextOptions);
+
+            repository = new UnitOfWork(context);
+        }
+    }
+}
